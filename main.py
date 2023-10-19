aa93b88a57767cc512a33797ae7f58428fdef010
Smaron47 <87486349+Smaron47@users.noreply.github.com>
2023-10-19 15:40:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,103 @@
 import os
 import json
-from flask import Flask, request, jsonify, render_template
-from lxml import html
+from flask import Flask, request, jsonify, render_template, send_from_directory
+
 import requests
 from datetime import datetime
 from google.oauth2 import service_account
 import gspread
+import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 app = Flask(__name__)
 
-# Google Sheets credentials (replace with your JSON file)
-scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://spreadsheets.google.com/feeds',
-        'https://www.googleapis.com/auth/drive']
-creds = service_account.Credentials.from_service_account_file('credet.json', scopes=scope)
-gc = gspread.authorize(creds)
 
-# Open the Google Sheet by its title
-sheet1 = gc.open_by_url("https://docs.google.com/spreadsheets/d/1K9CewgslVHqLqy9wwpazswR4jMk8mMK7hBedgd8dIDY/edit#gid=721899723")
-sheet = sheet1.worksheet("WebScrap")
-# Store scheduled jobs
-scheduled_jobs = {}
-row_index = 2  # Start from the second row (1st row for headers)
-urls_data = []
 
-# Load previously added URLs data from the JSON file
-try:
+
+# Initialize Google Sheets
+def init_google():
+    # Google Sheets credentials (replace with your JSON file)
+    scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://spreadsheets.google.com/feeds',
+            'https://www.googleapis.com/auth/drive']
+    creds = service_account.Credentials.from_service_account_file('credet.json', scopes=scope)
+    gc = gspread.authorize(creds)
+
+    # Open the Google Sheet by its title
+    sheet1 = gc.open_by_url("https://docs.google.com/spreadsheets/d/1K9CewgslVHqLqy9wwpazswR4jMk8mMK7hBedgd8dIDY/edit#gid=721899723")
+    sheet = sheet1.worksheet("WebScrap")
+    return sheet
+
+
+def append_column(sheet, data):
+    # Get the values in the first row to determine the last column with text
+    first_row = sheet.row_values(2)
+    last_col_index = len(first_row) + 1
+
+    # Append a new column at the end
+    sheet.add_cols(1)
+
+    # Update the header cell in the new column
+    #sheet.update_cell(1, last_col_index, datetime.today())
+
+    # Update the cells in the new column with data
+    for i, data_row in enumerate(data):
+        sheet.update_cell(i + 2, last_col_index, data_row)
+
+# Function to check urls_data.json and update the Google Sheet
+def update_google_sheet():
+    # Load data from urls_data.json
     with open('urls_data.json', 'r') as json_file:
         urls_data = json.load(json_file)
-except FileNotFoundError:
-    urls_data = []  # Create an empty list if the file doesn't exist
 
-def scrape_and_update_sheet(url, xpath, index):
-    # Create a headless Selenium WebDriver
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
+    # Initialize Google Sheets
+    sheet = init_google()
 
-    try:
-        driver.get(url)
-        elements = driver.find_elements(By.XPATH, xpath)
+    for data in urls_data:
+        url = data['url']
+        data_value = data['data']
 
-        if index < len(elements):
-            extracted_text = elements[index].text
-            print(f"Extracted text: {extracted_text} from URL: {url} at {datetime.now()}")
-        else:
-            extracted_text = driver.find_element(By.XPATH, xpath).text
-        print(extracted_text)
-        
-        # Check if the URL is in urls_data.json
-        url_exists = any(item['url'] == url for item in urls_data)
+        # Check if the URL is in the Google Sheet
+        try:
+            cell = sheet.find(url)
+            col_index = cell.col
+        except gspread.exceptions.CellNotFound:
+            print(f"URL '{url}' not found in the Google Sheet")
+            continue
 
-        # If the URL is not in urls_data.json or XPath doesn't match, create a new row
-        if not url_exists or any(item['url'] == url and item['xpath'] != xpath for item in urls_data):
-            # Update Google Sheet with URL, time, and extracted data
-            data_to_update = [url, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), extracted_text]
-            sheet.insert_row(data_to_update, row_index)
+        append_column(sheet,data_value)
 
-            # Store the data in the urls_data list
-            if url_exists:
-                urls_data = [item for item in urls_data if item['url'] != url]
-            urls_data.append({
-                'url': url,
-                'xpath': xpath,
-                'text_content': extracted_text
-            })
-
-    except Exception as e:
-        print(f"Error while scraping '{url}': {str(e)}")
-    finally:
-        driver.quit()
 
 # Initialize a BackgroundScheduler
 scheduler = BackgroundScheduler()
 scheduler.start()
+urls_data = []
+scheduled_jobs = {}
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    TIME=datetime.now()
+    global urls_data
+    TIME = datetime.now()
     if request.method == 'POST':
         data = request.form
         url = data['url']
         xpath = data['xpath']
         index = int(data.get('index', 0))  # Default index is 0 if not specified
         time_str = data['time']
-
+        urls_data.append({'url': url, 'time': time_str, 'index':index,'xpath':xpath,'data':'0'})
+        with open('urls_data.json', 'w') as pre_json_file:
+            json.dump(urls_data, pre_json_file)
+        sheet = init_google()
+        sheet.insert_row([url,xpath,time_str],2)
         try:
             # Parse the time input from the form
             scheduled_time = datetime.strptime(time_str, '%H:%M').time()
-            print(type(scheduled_time.hour), scheduled_time)
             # Create a job with a CronTrigger to run at the specified time daily
             trigger = CronTrigger(hour=scheduled_time.hour, minute=scheduled_time.minute)
-            scheduled_job = scheduler.add_job(scrape_and_update_sheet, trigger=trigger, args=[url, xpath, index])
+            
+            scheduled_job = scheduler.add_job(update_google_sheet, trigger=trigger)
 
             # Store the scheduled job and URL in the respective lists
             scheduled_jobs[(url, xpath, index)] = scheduled_job
@@ -104,19 +107,46 @@ def index():
             return jsonify({'error': 'Invalid time format. Please use HH:MM.'}), 400
 
     # Display the index page with a form to input schedules
-    return render_template('index.html',time=TIME)
+    return render_template('index.html', time=TIME)
 
-@app.route('/view_schedules', methods=['GET'])
-def view_schedules():
-    # Retrieve and display the existing schedules
-    schedules = [{'url': job.args[0], 'xpath': job.args[1], 'index': job.args[2], 'time': str(job.trigger.fields[1])} for job in scheduler.get_jobs()]
-    return jsonify({'schedules': schedules})
 
-@app.route("/fuck", methods=['GET','POST'])
+@app.route("/fuck", methods=['GET', 'POST'])
 def fuck():
     return render_template('fuck.html')
 
 
+# Additional server-side endpoints for downloading and uploading 'pre.json' file
+@app.route('/download_pre_json', methods=['GET'])
+def download_pre_json():
+    try:
+        # Serve the pre.json file for download
+        return jsonify(urls_data)
+    except FileNotFoundError:
+        return jsonify({'error': 'pre.json file not found'}), 404
+
+@app.route('/upload_pre_json', methods=['POST'])
+def upload_pre_json():
+    global urls_data
+    try:
+        # Handle JSON data sent in the request
+        uploaded_data = request.get_json()
+        if not uploaded_data or "url" not in uploaded_data or "data" not in uploaded_data:
+            return jsonify({'error': 'Invalid JSON data format. Expected { "url": "your_url", "data": "your_data" }'}), 400
+
+        # Find the URL in the urls_data list and update its "data" value
+        for item in urls_data:
+            if item["url"] == uploaded_data["url"]:
+                item["data"] = uploaded_data["data"]
+                break
+        
+
+        # Save the updated data to urls_data.json
+        with open("urls_data.json", "w") as f:
+            json.dump(urls_data, f)
+
+        return jsonify({'message': f'Data for URL "{uploaded_data["url"]}" updated successfully'})
+    except Exception as e:
+        return jsonify({'error': f'Error while processing JSON data: {str(e)}'}), 500
 
 if __name__ == '__main__':
     app.run()
